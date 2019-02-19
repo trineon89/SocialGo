feee8ac56b7524e632dbf03d71cbeb3450c65a1e
Unknown <trineon89@gmail.com>
2019-02-19 16:25:11 +0100

    Xamarin base build
    --empty build

--- node_modules/cordova-plugin-appcenter-shared/src/ios/AppCenterSharedPlugin.h
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-#import <Cordova/CDVPlugin.h>
-
-@interface AppCenterSharedPlugin : CDVPlugin
-
-- (void)pluginInitialize;
-- (void)getInstallId:(CDVInvokedUrlCommand *)command;
-
-@end
--- plugins/cordova-plugin-appcenter-shared/www/AppCenter.js
+++
@@ -1,9 +0,0 @@
-var exec = require('cordova/exec');
-
-var AppCenter = {
-    getInstallId: function (success, error) {
-        exec(success, error, "AppCenterShared", "getInstallId", []);
-    }
-};
-
-module.exports = AppCenter;
